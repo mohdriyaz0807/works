09851eaddb34363abc799ec3b40b9e8330b50946
Mohamed Riyaz <72142827+mohdriyaz0807@users.noreply.github.com>
2020-11-22 20:45:00 +0530

    Add files via upload

---
+++ day22/index.js
@@ -0,0 +1,192 @@
+var Cricket = /** @class */ (function () {
+    function Cricket() {
+        this.min = 0;
+        this.max = 6;
+        this.teamA = 1;
+    }
+    Cricket.prototype.getRuns = function (minruns, maxruns) {
+        this.max = maxruns;
+        this.min = minruns;
+        return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+    };
+    Cricket.prototype.timmer = function (team) {
+        this.teamA = team;
+        setTimeout("hit" + team + "()", 60000);
+    };
+    Cricket.prototype.countDown = function () {
+        var x = 60;
+        var timer = setInterval(function () {
+            document.getElementById("countDown").innerHTML = "" + --x;
+            if (x == 0) {
+                clearInterval(timer);
+            }
+        }, 1000);
+    };
+    return Cricket;
+}());
+var cricket = new Cricket();
+var hit1 = function () {
+    var players1 = ['Shane Watson', 'Faf du Plessis', 'Ambati Rayudu', 'Sam Curran', 'MS Dhoni', 'Dwayne Bravo', 'Ravindra Jadeja', 'N Jagadeesan', 'Karn Sharma', 'Shardul Thakur'];
+    var final = 0;
+    var max = [];
+    var highScore = [];
+    var _loop_1 = function (j) {
+        var tr = document.createElement("tr");
+        var total = 0;
+        var td = document.createElement("td");
+        td.innerHTML = players1["" + j];
+        tr.append(td);
+        var table1 = document.getElementById("tbody2");
+        var trow = table1.querySelectorAll("tr");
+        var tableLen = [];
+        trow.forEach(function (e) {
+            tableLen.push(e.querySelectorAll("td").length);
+        });
+        var check = 0;
+        for (var i = 1; i <= 6; i++) {
+            var run = cricket.getRuns(0, 6);
+            total += run;
+            var td_1 = document.createElement("td");
+            td_1.id = "td";
+            if (run == 0) {
+                td_1.innerHTML = '<b>OUT</b>';
+            }
+            else {
+                td_1.innerHTML = "" + run;
+            }
+            tr.append(td_1);
+            if (run == 0) {
+                check = i;
+                break;
+            }
+        }
+        if ((6 - check || 6 - check == 0) && check != 0) {
+            for (var k = 1; k <= 6 - check; k++) {
+                var td_2 = document.createElement("td");
+                tr.appendChild(td_2);
+            }
+        }
+        td = document.createElement("td");
+        td.innerHTML = "" + total;
+        tr.append(td);
+        document.getElementById("tbody1").append(tr);
+        final += total;
+        document.getElementById("scoreA").innerHTML = "" + final;
+        highScore.push(total);
+        max = Math.max.apply(Math, highScore);
+    };
+    for (var j = 0; j < 10; j++) {
+        _loop_1(j);
+    }
+    var maxScore = max;
+    var index = highScore.indexOf(max);
+    (document.getElementById("player")).innerHTML = players1["" + index];
+    (document.getElementById("scoreMax")).innerHTML = "SCORE " + maxScore;
+    document.getElementById("hit1").disabled = true;
+    document.getElementById("hit2").disabled = false;
+};
+var hit2 = function () {
+    var players2 = ['Rohit Sharma', ' Hardik Pandya', ' Jasprit Bumrah', ' Krunal Pandya', ' Ishan Kishan', ' Surya Kumar Yadav', ' Rahul Chahar', ' Anmolpreet Singh', ' Jayant Yadav', ' Quinton de Kock', ' Kieron Pollard'];
+    var final = 0;
+    var max = [];
+    var highScore = [];
+    var _loop_2 = function (j) {
+        var tr = document.createElement("tr");
+        var total = 0;
+        var td = document.createElement("td");
+        td.innerHTML = players2["" + j];
+        tr.append(td);
+        var table1 = document.getElementById("tbody1");
+        var trow = table1.querySelectorAll("tr");
+        var tableLen = [];
+        trow.forEach(function (e) {
+            tableLen.push(e.querySelectorAll("td").length);
+        });
+        var check = 0;
+        for (var i = 1; i <= 6; i++) {
+            var run = cricket.getRuns(0, 6);
+            total += run;
+            var td_3 = document.createElement("td");
+            td_3.id = "td";
+            if (run == 0) {
+                td_3.innerHTML = '<b>OUT</b>';
+            }
+            else {
+                td_3.innerHTML = "" + run;
+            }
+            tr.append(td_3);
+            if (run == 0) {
+                check = i;
+                break;
+            }
+        }
+        if ((6 - check || 6 - check == 0) && check != 0) {
+            for (var k = 1; k <= 6 - check; k++) {
+                var td_4 = document.createElement("td");
+                tr.appendChild(td_4);
+            }
+        }
+        td = document.createElement("td");
+        td.innerHTML = "" + total;
+        tr.append(td);
+        document.getElementById("tbody2").append(tr);
+        final += total;
+        document.getElementById("scoreB").innerHTML = "" + final;
+        highScore.push(total);
+        max = Math.max.apply(Math, highScore);
+    };
+    for (var j = 0; j < 10; j++) {
+        _loop_2(j);
+    }
+    var maxScore = max;
+    var index = highScore.indexOf(max);
+    (document.getElementById("player1")).innerHTML = players2["" + index];
+    (document.getElementById("scoreMax1")).innerHTML = "SCORE " + maxScore;
+    document.getElementById("hit2").disabled = true;
+    document.getElementById("result").disabled = false;
+};
+var h1 = function () {
+    var cricket = new Cricket();
+    cricket.timmer(1);
+    cricket.countDown();
+};
+var h2 = function () {
+    var cricket = new Cricket();
+    cricket.timmer(2);
+    cricket.countDown();
+};
+var generateResult = function () {
+    var score1 = document.getElementById("scoreA").innerText;
+    var score2 = document.getElementById("scoreB").innerText;
+    if (+score1 > +score2) {
+        document.getElementById("team").innerText = "TEAM 1";
+        document.getElementById("team2").innerText = "TEAM 1";
+    }
+    else {
+        document.getElementById("team").innerText = "TEAM 2";
+        document.getElementById("team2").innerText = "TEAM 2";
+    }
+    document.getElementById("resultBox").style.display =
+        "block";
+    var winner = document.getElementById("team");
+    if (winner.innerHTML == "TEAM 1") {
+        document.getElementById("player").style.display =
+            "block";
+        document.getElementById("player1").style.display =
+            "none";
+        document.getElementById("scoreMax").style.display =
+            "block";
+        document.getElementById("scoreMax1").style.display =
+            "none";
+    }
+    else {
+        document.getElementById("player").style.display =
+            "none";
+        document.getElementById("player1").style.display =
+            "block";
+        document.getElementById("scoreMax").style.display =
+            "none";
+        document.getElementById("scoreMax1").style.display =
+            "block";
+    }
+};
---
+++ day22/index.ts
@@ -0,0 +1,201 @@
+class Cricket {
+    runs: number;
+    min: number;
+    max: number;
+    teamA: number;
+    constructor() {
+      this.min = 0;
+      this.max = 6;
+      this.teamA = 1;
+    }
+    getRuns(minruns: number, maxruns: number) {
+      this.max = maxruns;
+      this.min = minruns;
+      return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+    }
+    timmer(team: number) {
+      this.teamA = team;
+      setTimeout(`hit${team}()`, 60000);
+    }
+    countDown() {
+      let x = 60;
+      let timer = setInterval(function () {
+        document.getElementById("countDown").innerHTML = `${--x}`;
+        if (x == 0) {
+          clearInterval(timer);
+        }
+      },1000);
+    }
+  }
+  let cricket = new Cricket();
+  let hit1 = () => {
+    let players1=['Shane Watson','Faf du Plessis','Ambati Rayudu','Sam Curran','MS Dhoni','Dwayne Bravo','Ravindra Jadeja','N Jagadeesan','Karn Sharma','Shardul Thakur']
+    let final = 0;
+    let max: any = [];
+    let highScore = [];
+    for (let j = 0; j < 10; j++) {
+      let tr = document.createElement("tr");
+      let total = 0;
+      let td = document.createElement("td");
+      td.innerHTML = players1[`${j}`];
+      tr.append(td);
+      let table1 = document.getElementById("tbody2");
+      let trow = table1.querySelectorAll("tr");
+      let tableLen = [];
+      trow.forEach(function (e) {
+        tableLen.push(e.querySelectorAll("td").length);
+      });
+      let check = 0;
+      for (let i = 1; i <= 6; i++) {
+        let run = cricket.getRuns(0, 6);
+        total += run;
+        let td = document.createElement("td");
+        td.id = "td";
+        if(run==0){
+          td.innerHTML = '<b>OUT</b>'}
+          else{
+        td.innerHTML = `${run}`;
+          }
+        tr.append(td);
+        if (run == 0) {
+          check = i;
+          break;
+        }
+      }
+      if ((6 - check || 6 - check == 0) && check != 0) {
+        for (let k = 1; k <= 6 - check; k++) {
+          let td = document.createElement("td");
+          tr.appendChild(td);
+        }
+      }
+      td = document.createElement("td");
+      td.innerHTML = `${total}`;
+      tr.append(td);
+      document.getElementById("tbody1").append(tr);
+      final += total;
+      document.getElementById("scoreA").innerHTML = `${final}`;
+      highScore.push(total);
+      max = Math.max(...highScore);
+    }
+    let maxScore = max;
+    let index = highScore.indexOf(max) ;
+    (<HTMLInputElement>(
+      document.getElementById("player")
+    )).innerHTML = players1[`${index}`];
+    (<HTMLInputElement>(
+      document.getElementById("scoreMax")
+    )).innerHTML = `SCORE ${maxScore}`;
+  
+    (<HTMLInputElement>document.getElementById("hit1")).disabled = true;
+    (<HTMLInputElement>document.getElementById("hit2")).disabled = false;
+  };
+  let hit2 = () => {
+    let players2=['Rohit Sharma',' Hardik Pandya',' Jasprit Bumrah',' Krunal Pandya',' Ishan Kishan',' Surya Kumar Yadav',' Rahul Chahar',' Anmolpreet Singh',' Jayant Yadav',' Quinton de Kock',' Kieron Pollard']
+    let final = 0;
+    let max: any = [];
+    let highScore = [];
+    for (let j = 0; j < 10; j++) {
+      let tr = document.createElement("tr");
+      let total = 0;
+      let td = document.createElement("td");
+      td.innerHTML = players2[`${j}`];
+      tr.append(td);
+      let table1 = document.getElementById("tbody1");
+      let trow = table1.querySelectorAll("tr");
+      let tableLen = [];
+      trow.forEach(function (e) {
+        tableLen.push(e.querySelectorAll("td").length);
+      });
+      let check = 0;
+  
+      for (let i = 1; i <= 6; i++) {
+        let run = cricket.getRuns(0, 6);
+        total += run;
+  
+        let td = document.createElement("td");
+        td.id = "td";
+        if(run==0){
+          td.innerHTML = '<b>OUT</b>'}
+          else{
+        td.innerHTML = `${run}`;
+          }
+        tr.append(td);
+        if (run == 0) {
+          check = i;
+          break;
+        }
+      }
+      if ((6 - check || 6 - check == 0) && check != 0) {
+        for (let k = 1; k <= 6 - check; k++) {
+          let td = document.createElement("td");
+          tr.appendChild(td);
+        }
+      }
+      td = document.createElement("td");
+      td.innerHTML = `${total}`;
+      tr.append(td);
+      document.getElementById("tbody2").append(tr);
+      final += total;
+      document.getElementById("scoreB").innerHTML = `${final}`;
+      highScore.push(total);
+      max = Math.max(...highScore);
+    }
+    let maxScore = max;
+    let index = highScore.indexOf(max) ;
+    (<HTMLInputElement>(
+      document.getElementById("player1")
+    )).innerHTML = players2[`${index}`];
+    (<HTMLInputElement>(
+      document.getElementById("scoreMax1")
+    )).innerHTML = `SCORE ${maxScore}`;
+  
+    (<HTMLInputElement>document.getElementById("hit2")).disabled = true;
+    (<HTMLInputElement>document.getElementById("result")).disabled = false;
+  
+  };
+  let h1 = () => {
+    let cricket = new Cricket();
+    cricket.timmer(1);
+    cricket.countDown();
+  
+  };
+  let h2 = () => {
+    let cricket = new Cricket();
+    cricket.timmer(2);
+    cricket.countDown();
+  };
+  let generateResult = () => {
+    let score1 = document.getElementById("scoreA").innerText;
+    let score2 = document.getElementById("scoreB").innerText;
+    if (+score1 > +score2) {
+      document.getElementById("team").innerText = "TEAM 1";
+      document.getElementById("team2").innerText = "TEAM 1";
+    } else {
+      document.getElementById("team").innerText = "TEAM 2";
+      document.getElementById("team2").innerText = "TEAM 2";
+    }
+    (<HTMLInputElement>document.getElementById("resultBox")).style.display =
+      "block";
+  
+    let winner = document.getElementById("team");
+    if (winner.innerHTML == `TEAM 1`) {
+      (<HTMLInputElement>document.getElementById("player")).style.display =
+        "block";
+      (<HTMLInputElement>document.getElementById("player1")).style.display =
+        "none";
+      (<HTMLInputElement>document.getElementById("scoreMax")).style.display =
+        "block";
+      (<HTMLInputElement>document.getElementById("scoreMax1")).style.display =
+        "none";
+    } else {
+      (<HTMLInputElement>document.getElementById("player")).style.display =
+        "none";
+      (<HTMLInputElement>document.getElementById("player1")).style.display =
+        "block";
+  
+      (<HTMLInputElement>document.getElementById("scoreMax")).style.display =
+        "none";
+      (<HTMLInputElement>document.getElementById("scoreMax1")).style.display =
+        "block";
+    }
+  };
